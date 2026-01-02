f93113f3fce02c9806d7146c0419ecbeca47d1f6
Leona Redmann <leored@gmx.de>
2026-01-02 23:16:07 +0100

    feat(textureDevider): optimize the look and add a text to the devider

--- src/components/sections/TextureDivider.tsx
+++ src/components/sections/TextureDivider.tsx
@@ -3,11 +3,14 @@ import textureImg from "../../assets/texture.png";
 
 export default function TextureDivider() {
     return (
-        <div className="py-20 opacity-35">
+        <div className="relative py-20">
+            <h1 className="absolute inset-0 flex items-center justify-center font-serif text-3xl md:text-5xl leading-tight text-white z-10 opacity-90">
+                Radiant skin for your <span className="italic">special moments.</span>
+            </h1>
             <ImageCard
                 src={textureImg}
                 alt="Lunara Skin texture detail"
-                className="aspect-5/2 md:aspect-3/1 w-full "
+                className="aspect-5/2 md:aspect-3/1 w-full opacity-60"
             />
         </div>
     );
